5c5dbd2b11491a4efec52f9c2b5d090302d77beb
joaofavara <joaopedro_favara@hotmail.com>
2023-10-15 19:50:25 -0300

    feat: setup sonar cloud

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Starting the project creation"
+
+gh repo create {{ cookiecutter.github_org }}/{{ cookiecutter.project_slug }} --public
+git init
+
+# nvm use
+
+sleep 60
+
+git branch -M master
+git add .
+git commit -m "feat: start repository"
+git remote add origin git@github.com:joaofavara/learning-cookiecutter.git
+git push -u origin master
+
+gh secret set SONAR_TOKEN -b {{ cookiecutter.sonnarcloud_token }}
+
+curl --include \
+     --request POST \
+"https://{{ cookiecutter.sonnarcloud_token}}@sonarcloud.io/api/projects/create?newCodeDefinitionType=days&newCodeDefinitionValue=60&project={{ cookiecutter.github_org }}_{{ cookiecutter.project_slug }}&organization={{ cookiecutter.github_org }}&name={{ cookiecutter.project_slug }}"
+
+code .
+
+echo "Finish the project creation"
+
+
