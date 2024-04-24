ba3ee99e4791f3b6956a2c6f348b85f9a5e88bb8
Joao Favara <joaopedro_favara@hotmail.com>
2024-04-23 21:18:41 -0300

    feat: stash

--- {{ cookiecutter.project_name }}/test/index.test.js
+++ {{ cookiecutter.project_name }}/test/index.test.js
@@ -1,7 +1,10 @@
+const { describe, test } = require('node:test');
+const assert = require('node:assert');
+
 const { dummy } = require('../src');
 
 describe('Dummy Test', () => {
     test('should return string "dummy"', () => {
-        expect(dummy()).toBe('dummy');
+        assert.strictEqual(dummy(), 'dummy');
     });
 });
